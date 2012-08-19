1ede3a2291971d5a2d94c96a2a2d282caf992df1
Fabio Albertin <albertin@gmail.com>
2012-08-19 22:36:19 +0200

    Converted SASS to SCSS syntax

--- config.rb
+++ config.rb
@@ -14,7 +14,6 @@ javascripts_dir       = 'public/javascripts'
 
 # Set to true for easier debugging
 line_comments         = false
-preferred_syntax      = :sass
 
 # CSS output style - :nested, :expanded, :compact, or :compressed
 output_style          = :expanded
